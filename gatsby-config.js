54bc9282b8e5318fa89bc5bec0d106b1f955cba0
Eric Schreiter <mail@shter.de>
2019-03-01 16:23:02 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,19 +5,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Eric Schreiter",
-        short_name: "shter.dev",
-        start_url: "/",
-        background_color: "#0C0C0C",
-        theme_color: "#0C0C0C",
+        name: `Eric Schreiter`,
+        short_name: `shter.dev`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
       },
     },
     {
